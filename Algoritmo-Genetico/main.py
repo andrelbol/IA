35e03418b31114c04764314eee24e1cbab77b12c
Andre Borges <andreluiborges1@gmail.com>
2018-06-07 23:00:46 -0300

    Pequena modificação da função objetivo

--- Algoritmo-Genetico/main.py
+++ Algoritmo-Genetico/main.py
@@ -5,11 +5,10 @@ def objective_function(*args):
   x1 = args[0]
   x2 = args[1]
   func = -(x2 + 47)*math.sin(math.sqrt(abs(x2 + x1/2 + 47))) - x1*math.sin(math.sqrt(abs(x1 - (x2 + 47))))
-  return func + 959.6407
+  return -1*func + 1500
 
 def select_one_chrom_index(population_fitness, total_sum):
   random_number = random.uniform(0, total_sum)
-  print(random_number)
   fitness_sum = 0
   for i in range(0, population_fitness.shape[0]):
     fitness_sum += population_fitness[i]
@@ -24,8 +23,7 @@ def select(population):
   population_count = population.shape[0]
   population_size = population.shape[1]
   population_fitness = np.zeros([population_count])
-  new_population_count = math.floor(population_count*0.8)
-  new_population = np.zeros([new_population_count, population_size])
+  new_population = np.zeros([population_count, population_size])
 
   total_sum = 0
   for i in range(0, population_count):
@@ -33,7 +31,7 @@ def select(population):
     population_fitness[i] = fitness
     total_sum += fitness
 
-  for i in range(0, new_population_count):
+  for i in range(0, population_count):
     index = select_one_chrom_index(population_fitness, total_sum)
     new_population[i] = population[index]
 
@@ -64,9 +62,10 @@ def mutate(population):
 
 def main():
   population = generate_population()
-  crossed_population = crossover(population)
-  selected_population = select(crossed_population)
-  print(selected_population)
+  for i in range(0, 1000):
+    population = crossover(population)
+    population = select(population)
+  print(population)
   return
   
 
